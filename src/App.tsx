2bfe96f592c0d7247d88b2020e8c0d0ad1447183
takuya_kajiwara <fd.elt.119@gmail.com>
2022-08-24 22:41:49 +0900

    input機能実装

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,12 @@ import { TodoList } from "./TodoList";
 
 export const App = (): JSX.Element => {
   return (
-    <>
-      <body>
+      <div>
         <header>
           <h1>~ React TODO of Typescript ~</h1>
         </header>
         <Form />
         <TodoList />
-      </body>
-    </>
+      </div>
   );
 };
--- src/Form.tsx
+++ src/Form.tsx
@@ -1,31 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
 export const Form = () => {
+
+  const [inputValue, setInputValue] = useState("")
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    setInputValue("")
+  }
+
+
   return (
     <div className="container-input">
-      <Box
-        component="form"
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
+      <form onSubmit={(e) => handleSubmit(e)}>
         <TextField
           label="add your TODO"
           id="filled-size-normal"
-          defaultValue=""
           variant="standard"
+          value={inputValue}
+          onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setInputValue(e.target.value)}
+
         />
-      </Box>
 
       <Stack spacing={2} direction="row" className="addButton">
         <Button variant="contained">add TODO</Button>
       </Stack>
+      </form>
+      <li>{inputValue}</li>
     </div>
   );
 };
